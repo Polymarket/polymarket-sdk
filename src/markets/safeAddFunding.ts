26cd8ba9bbce418f98fb3bff28b8e6ecebbe7642
Tom French <tom@tomfren.ch>
2021-04-01 17:48:40 +0100

    feat: add a function for safe provision of liquidity

---
+++ src/markets/safeAddFunding.ts
@@ -0,0 +1,55 @@
+import { Interface } from "@ethersproject/abi";
+import { BigNumberish } from "@ethersproject/bignumber";
+import { erc20ApprovalTransaction } from "../utils";
+import { CallType, Transaction } from "../types";
+
+const encodeSafeAddFunding = (
+  marketMakerAddress: string,
+  investmentAmount: BigNumberish,
+  distributionHint: BigNumberish[],
+  positionId: BigNumberish,
+  minRefund: BigNumberish,
+): string =>
+  new Interface([
+    "function addFunding(address,uint256,uint256[] memory,uint256,uint256)",
+  ]).encodeFunctionData("addFunding(address,uint256,uint256[] memory,uint256,uint256)", [
+    marketMakerAddress,
+    investmentAmount,
+    distributionHint,
+    positionId,
+    minRefund,
+  ]);
+
+const safeAddFundingTransaction = (
+  slippageCheckerAddress: string,
+  marketMakerAddress: string,
+  investmentAmount: BigNumberish,
+  distributionHint: BigNumberish[],
+  positionId: BigNumberish,
+  minRefund: BigNumberish,
+): Transaction => ({
+  to: slippageCheckerAddress,
+  typeCode: CallType.DelegateCall,
+  data: encodeSafeAddFunding(marketMakerAddress, investmentAmount, distributionHint, positionId, minRefund),
+  value: "0",
+});
+
+export const safeAddFundingToMarket = (
+  slippageCheckerAddress: string,
+  marketMakerAddress: string,
+  collateralTokenAddress: string,
+  investmentAmount: BigNumberish,
+  distributionHint: BigNumberish[] = [],
+  positionId: BigNumberish,
+  minRefund: BigNumberish,
+): Transaction[] => [
+  erc20ApprovalTransaction(collateralTokenAddress, marketMakerAddress, investmentAmount),
+  safeAddFundingTransaction(
+    slippageCheckerAddress,
+    marketMakerAddress,
+    investmentAmount,
+    distributionHint,
+    positionId,
+    minRefund,
+  ),
+];
